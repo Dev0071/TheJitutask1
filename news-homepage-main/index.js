39fa7d13d27c7b0f100a7387290b4fa78b4f3b9d
Dev0071 <lecturergacheru71@gmail.com>
2023-07-10 14:14:08 +0300

    news landing page challeng

---
+++ news-homepage-main/index.js
@@ -0,0 +1,12 @@
+// alert('working');
+const nav = document.querySelector('nav');
+const openMenu = document.getElementById('menu-button');
+const closeMenu = document.getElementById('close-button');
+
+openMenu.addEventListener('click', () => {
+	nav.classList.add('open');
+});
+
+closeMenu.addEventListener('click', () => {
+	nav.classList.remove('open');
+});
