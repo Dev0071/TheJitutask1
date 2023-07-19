99690759cae47b64b9ae47a74c301b822b8949e8
Dev0071 <lecturergacheru71@gmail.com>
2023-07-19 13:53:06 +0300

    post-counter and show-comments functiobality

--- Twitterclone/index.js
+++ Twitterclone/index.js
@@ -2,6 +2,7 @@ const selectInput = document.getElementById('select-input');
 const userDetailsDiv = document.getElementById('user-details');
 const postDetailsDiv = document.getElementById('post-details');
 const commentsDiv = document.getElementById('comments');
+const postCounter = document.getElementById('post-counter');
 
 // fetch  function
 const fetchFunction = async url => {
@@ -32,7 +33,7 @@ const displayPosts = async userId => {
 	const userPosts = posts.filter(post => post.userId === parseInt(userId));
 	postDetailsDiv.innerHTML = '';
 	userPosts.forEach(post => {
-		const postTemplate = `<article class="post">
+		const postTemplate = `<article class="post" data-post-Id=${post.id} onclick="displayComments(${post.id})">
             <img src="https://www.gravatar.com/avatar/2c7d99fe281ecd3bcd65ab915bac6dd5?s=250">
             <div class="post-info">
                 <p>${user.name}<img src="./images/Twitter_Verified_Badge.svg.png" alt=""> <img
@@ -66,23 +67,35 @@ const displayUsers = async () => {
 		option.textContent = user.name;
 		selectInput.appendChild(option);
 	});
+
+	// displays the first user data on initial render
 	if (users.length > 0) {
 		const firstUserId = users[0].id;
 		displayUserData(firstUserId);
 		displayPosts(firstUserId);
 	}
+	// display the comments for the first post of the first user on initial render
+	setTimeout(() => {
+		const firstPost = document.querySelector('.post');
+		if (firstPost) {
+			displayComments(firstPost.dataset.postId);
+		}
+	}, 100);
 };
 
 const displayComments = async postId => {
+	commentsDiv.innerHTML = '';
 	const comments = await fetchFunction('https://jsonplaceholder.typicode.com/comments');
 	const postComments = comments.filter(comment => comment.postId === parseInt(postId));
+	postCounter.textContent = `Post ${postId} Comments`;
 	// console.log(postComments);
 	postComments.forEach(comment => {
-		const commentTemplate = `<article class="post">
+		const commentTemplate = `
+		<article class="post">
             <img src="https://www.gravatar.com/avatar/2c7d99fe281ecd3bcd65ab915bac6dd5?s=250">
             <div class="post-info">
                 <p>${comment.name}<img src="./images/Twitter_Verified_Badge.svg.png" alt=""> <img
-                        src="./images/twitter-logo.png" alt="">
+                src="./images/twitter-logo.png" alt="">
                 </p>
                 <p>${comment.body}</p>
                 <div id="reaction">
@@ -90,13 +103,14 @@ const displayComments = async postId => {
                         <img src="./images/comment-icon.png" alt="">
                         <p>0</p>
                     </span>
-                    <span><img src="./images/retweet-11.png" alt="">
+                    <span>
+                        <img src="./images/retweet-11.png" alt="">
                         <p>20</p>
                     </span>
-                    <span><img src="./images/like-icon.png" alt="">
+                    <span>
+                        <img src="./images/like-icon.png" alt="">
                         <p>2</p>
                     </span>
-
                 </div>
             </div>
         </article>`;
@@ -107,8 +121,12 @@ const displayComments = async postId => {
 
 const displayData = async userId => {
 	displayUserData(userId);
-	displayPosts(userId);
+	await displayPosts(userId);
+	const userFirstPost = document.querySelector('.post');
+	console.log(userFirstPost);
+	if (userFirstPost) {
+		displayComments(userFirstPost.dataset.postId);
+	}
 };
 
 displayUsers();
-displayComments(1);
